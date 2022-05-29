261a53c8eced33c5256c8e1246777e9362657b40
PrandiFabiel <fabiel098@gmail.com>
2022-05-29 16:14:24 -0400

    Creando Registro y lista de personas.

---
+++ app/src/main/java/com/example/tarea3registroscompose/ui/theme/screens/PersonaListScreen.kt
@@ -0,0 +1,100 @@
+package com.example.tarea3registroscompose.ui.theme.screens
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.tarea3registroscompose.ui.theme.Tarea3RegistrosComposeTheme
+
+@Composable
+fun PersonaListScreen(
+    //onNavigateToPersona: () -> Unit,
+) {
+    val scaffoldState = rememberScaffoldState()
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Persona List") }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                     //onNavigateToPersona()
+                },
+                backgroundColor = MaterialTheme.colors.primary
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Nuevo")
+            }
+        },
+        scaffoldState = scaffoldState
+    ) {
+        Column(modifier = Modifier.padding(it)) {
+            Row(modifier = Modifier){
+                Text(
+                    text = "Nombre",
+                    modifier = Modifier.padding(start = 20.dp),
+                    style = MaterialTheme.typography.h5
+                )
+                Text(
+                    text = "Ocupacion",
+                    modifier = Modifier.padding(start = 140.dp),
+                    style = MaterialTheme.typography.h5
+                )
+            }
+            LazyColumn(modifier = Modifier.fillMaxWidth()) {
+                items(5) { index ->
+                    PersonaRow(name = "Fabiel", ocupacion = "Ingeniero", onClick = {})
+                }
+            }
+        }
+    }
+
+}
+
+
+@Composable
+fun PersonaRow(name: String, ocupacion: String, onClick: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(
+                enabled = true,
+                onClick = { })
+    )
+
+    {
+        Row(modifier = Modifier.padding(top = 10.dp)) {
+            Text(
+                text = name,
+                maxLines = 1,
+                modifier = Modifier.padding(start = 20.dp)
+            )
+            Text(
+                text = ocupacion,
+                modifier = Modifier.padding(start = 185.dp)
+            )
+        }
+
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Tarea3RegistrosComposeTheme {
+        PersonaListScreen()
+    }
+}
---
+++ app/src/main/java/com/example/tarea3registroscompose/ui/theme/screens/PersonaScreen.kt
@@ -0,0 +1,134 @@
+package com.example.tarea3registroscompose.ui.theme.screens
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.icons.filled.Save
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toSize
+import com.example.tarea3registroscompose.ui.theme.Tarea3RegistrosComposeTheme
+
+@Composable
+fun PersonaScreen(
+    modifier: Modifier = Modifier,
+) {
+    val scaffoldState = rememberScaffoldState()
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Registro Personas") }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+
+                },
+                backgroundColor = MaterialTheme.colors.primary
+            ) {
+                Icon(imageVector = Icons.Default.Save, contentDescription = "New")
+            }
+        },
+        scaffoldState = scaffoldState
+    ) {
+        Column(modifier = Modifier.padding(10.dp)) {
+            OutlinedTextField(
+                label = {
+                    Text(text = "Nombre")
+                },
+                value = "",
+                onValueChange = {  },
+                modifier = modifier.fillMaxWidth()
+            )
+            OutlinedTextField(
+                label = {
+                    Text(text = "Salario")
+                },
+                value = "",
+                onValueChange = { },
+                modifier =  modifier.fillMaxWidth()
+            )
+            OutlinedTextField(
+                label = {
+                    Text(text = "Email")
+                },
+                value = "",
+                onValueChange = { },
+                modifier =  modifier.fillMaxWidth()
+            )
+            Spinner()
+        }
+    }
+
+}
+
+@Composable
+fun Spinner(){
+
+    var mExpanded by remember { mutableStateOf(false) }
+
+    val ocupaciones = listOf("Ingeniero", "Doctor", "Abogado")
+
+    var mSelectedText by remember { mutableStateOf("") }
+
+    var mTextFieldSize by remember { mutableStateOf(Size.Zero)}
+
+    val icon = if (mExpanded)
+        Icons.Filled.KeyboardArrowUp
+    else
+        Icons.Filled.KeyboardArrowDown
+
+    Column(Modifier.padding(10.dp).fillMaxWidth()) {
+
+        OutlinedTextField(
+            value = mSelectedText,
+            onValueChange = { mSelectedText = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .onGloballyPositioned { coordinates ->
+                    mTextFieldSize = coordinates.size.toSize()
+                },
+            label = {Text("Ocupacion")},
+            trailingIcon = {
+                Icon(icon,"contentDescription",
+                    Modifier.clickable { mExpanded = !mExpanded })
+            }
+        )
+        DropdownMenu(
+            expanded = mExpanded,
+            onDismissRequest = { mExpanded = false },
+            modifier = Modifier
+                .width(with(LocalDensity.current){mTextFieldSize.width.toDp()})
+        ) {
+            ocupaciones.forEach { label ->
+                DropdownMenuItem(onClick = {
+                    mSelectedText = label
+                    mExpanded = false
+                }) {
+                    Text(text = label)
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewPersonaScreen(){
+    Tarea3RegistrosComposeTheme {
+        PersonaScreen()
+    }
+}
